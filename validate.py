56b6c4fdf0e200ca00e0a35d298f0bf47eb6ae92
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-10-23 20:46:41 +0300

    rewrite wer calculation in validation script

--- validate.py
+++ validate.py
@@ -1,13 +1,18 @@
 from src.models.utils import import_gigaam_model
-from utils import calculate_wer_on_dataset
+from utils import calculate_wer_on_dataset, calculate_wer
 from src.data.dataset import AudioDataset
 from src.data.utils import collate_fn
 
 from torch.utils.data import DataLoader
+import torch
 
 if __name__ == '__main__':
     #TODO: позже здесь нужно будет написать импорт модели с определенного чекпоинта
     model = import_gigaam_model()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.to(device)
 
     val_dataset = AudioDataset(preprocessor=None, dataset_part="validation")
     val_loader = DataLoader(
@@ -19,5 +24,10 @@ if __name__ == '__main__':
         # pin_memory=True if torch.cuda.is_available() else False
     )
 
-    wer = calculate_wer_on_dataset(model=model, dataloader=val_loader)
+    # wer = calculate_wer_on_dataset(model=model, dataloader=val_loader)
+    wer, _, _ = calculate_wer(
+        model=model,
+        dataloader=val_loader,
+        device=device
+    )
     print('WER on validation dataset is: ', wer)
